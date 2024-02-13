688044a861ca18b588bc051ebc816358a2cdeb0f
Mr.Cowley <96976330+gononono64@users.noreply.github.com>
2024-02-13 14:18:13 -0600

    Add files via upload

---
+++ mrc-template/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'mrc-template'
+version '1.0.0'
+
+server_scripts {
+    'server/*.lua',
+}
+
+client_scripts {
+    'client/*.lua',
+}
+
+lua54 'yes'
+
